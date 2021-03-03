26f0e4c86360d8534f724a5d10f0f5210d5f5819
AkylaiSh <aka47sharipova@gmail.com>
2021-03-02 21:14:12 -0500

    Complete assignment

--- index.js
+++ index.js
@@ -20,3 +20,7 @@ function shout(string){
             if (string === ("I love you, Grandma."))
         return "I love you, too.";
 }
+
+console.log(sayHiToGrandma('hello'));
+console.log(sayHiToGrandma('HELLO'));
+console.log(sayHiToGrandma("I love you, Grandma."));
